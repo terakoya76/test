8ba12e9dfaedae5c5072bfdf4ee6696c543313a7
Hajime Terasawa <terako.studio@gmail.com>
2023-07-07 15:42:00 +0900

    chore: test

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+  }
+
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
+resource "null_resource" "dummy" {
+  triggers = {}
+
+  provisioner "local-exec" {
+    command = "ls"
+  }
+}
